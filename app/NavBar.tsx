de3bf873fb254f59e4b403d1c2f99069465b839f
junaid-1993 <junaidali9001@gmail.com>
2024-06-08 13:08:45 +0500

    Style the active link in the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,11 +1,18 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { AiFillBug } from "react-icons/ai";
+import classnames from "classnames";
 
 const NavBar = () => {
+  // 'usePathname' is a Client Component hook that lets you read the current 'URL's' pathname.
+  const currentPath = usePathname();
+
   const links = [
     { label: "Dashboard", href: "/" },
-    { label: "Issues", href: "/issue" },
+    { label: "Issues", href: "/issues" },
   ];
   return (
     <nav className="flex space-x-6 mb-5 border-b px-5 h-14 items-center">
@@ -15,7 +22,17 @@ const NavBar = () => {
       <ul className="flex space-x-6">
         {links.map((link) => (
           <li key={link.href}>
-            <Link href={link.href} className="text-zinc-500 hover:text-zinc-800 transition-colors">
+            <Link
+              href={link.href}
+              // 'classnames' is a function that takes an object in this object we specify the 'classes' we want to render and
+              // the 'condition' in which they should be rendered. This will then return a 'string' that contains the 'classes' that
+              // we want to render.
+              className={classnames({
+                "text-zinc-900": link.href === currentPath,
+                "text-zinc-500": link.href !== currentPath,
+                "hover:text-zinc-800 transition-colors": true,
+              })}
+            >
               {link.label}
             </Link>
           </li>
