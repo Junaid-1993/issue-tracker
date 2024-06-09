f2968240ec5e08c5365bec5ecfddf69ed3df43e8
junaid-1993 <junaidali9001@gmail.com>
2024-06-09 18:08:53 +0500

    Extract the ErrorMessage component

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -10,6 +10,7 @@ import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { createIssueSchema } from "@/app/validationSchemas";
 import { z } from "zod";
+import ErrorMessage from "@/app/components/ErrorMessage";
 
 // Infer types from our schema
 type IssueForm = z.infer<typeof createIssueSchema>;
@@ -48,18 +49,10 @@ const NewIssuePage = () => {
         })}
       >
         <TextField.Root placeholder="Title" {...register("title")} />
-        {errors.title && (
-          <Text color="red" as="p">
-            {errors.title.message}
-          </Text>
-        )}
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
         {/* We will use the 'Controller' component to spread 'register' like properties which are in the 'field' property. */}
         <Controller name="description" control={control} render={({ field }) => <SimpleMDE placeholder="Description" {...field} />} />
-        {errors.description && (
-          <Text color="red" as="p">
-            {errors.description.message}
-          </Text>
-        )}
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
         <Button>Submit New Issue</Button>
       </form>
     </div>
