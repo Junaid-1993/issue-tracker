8b665131433d25e8cb5c76b1601c48a862b4bb8e
junaid-1993 <junaidali9001@gmail.com>
2024-06-25 12:39:33 +0500

    Change the layout of the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -6,7 +6,7 @@ import React from "react";
 import { AiFillBug } from "react-icons/ai";
 import classnames from "classnames";
 import { useSession } from "next-auth/react";
-import { Box } from "@radix-ui/themes";
+import { Box, Container, Flex } from "@radix-ui/themes";
 
 const NavBar = () => {
   // 'usePathname' is a Client Component hook that lets you read the current 'URL's' pathname.
@@ -20,33 +20,39 @@ const NavBar = () => {
     { label: "Issues", href: "/issues/list" },
   ];
   return (
-    <nav className="flex space-x-6 mb-5 border-b px-5 h-14 items-center">
-      <Link href="/">
-        <AiFillBug size="22px" />
-      </Link>
-      <ul className="flex space-x-6">
-        {links.map((link) => (
-          <li key={link.href}>
-            <Link
-              href={link.href}
-              // 'classnames' is a function that takes an object in this object we specify the 'classes' we want to render and
-              // the 'condition' in which they should be rendered. This will then return a 'string' that contains the 'classes' that
-              // we want to render.
-              className={classnames({
-                "text-zinc-900": link.href === currentPath,
-                "text-zinc-500": link.href !== currentPath,
-                "hover:text-zinc-800 transition-colors": true,
-              })}
-            >
-              {link.label}
+    <nav className="mb-5 border-b px-5 py-3">
+      <Container>
+        <Flex justify="between">
+          <Flex align="center" gap="3">
+            <Link href="/">
+              <AiFillBug size="22px" />
             </Link>
-          </li>
-        ))}
-      </ul>
-      <Box>
-        {status === "authenticated" && <Link href="/api/auth/signout">Log out</Link>}
-        {status === "unauthenticated" && <Link href="/api/auth/signin">Login</Link>}
-      </Box>
+            <ul className="flex space-x-6">
+              {links.map((link) => (
+                <li key={link.href}>
+                  <Link
+                    href={link.href}
+                    // 'classnames' is a function that takes an object in this object we specify the 'classes' we want to render and
+                    // the 'condition' in which they should be rendered. This will then return a 'string' that contains the 'classes' that
+                    // we want to render.
+                    className={classnames({
+                      "text-zinc-900": link.href === currentPath,
+                      "text-zinc-500": link.href !== currentPath,
+                      "hover:text-zinc-800 transition-colors": true,
+                    })}
+                  >
+                    {link.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </Flex>
+          <Box>
+            {status === "authenticated" && <Link href="/api/auth/signout">Log out</Link>}
+            {status === "unauthenticated" && <Link href="/api/auth/signin">Login</Link>}
+          </Box>
+        </Flex>
+      </Container>
     </nav>
   );
 };
