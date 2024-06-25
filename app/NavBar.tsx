035aeddad6c8bf9190915775502b42beed7c2740
junaid-1993 <junaidali9001@gmail.com>
2024-06-25 18:28:36 +0500

    Refactor the NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -9,16 +9,6 @@ import { useSession } from "next-auth/react";
 import { Avatar, Box, Container, DropdownMenu, Flex, Text } from "@radix-ui/themes";
 
 const NavBar = () => {
-  // 'usePathname' is a Client Component hook that lets you read the current 'URL's' pathname.
-  const currentPath = usePathname();
-
-  // Accessing the session:
-  const { status, data: session } = useSession();
-
-  const links = [
-    { label: "Dashboard", href: "/" },
-    { label: "Issues", href: "/issues/list" },
-  ];
   return (
     <nav className="mb-5 border-b px-5 py-3">
       <Container>
@@ -27,47 +17,74 @@ const NavBar = () => {
             <Link href="/">
               <AiFillBug size="22px" />
             </Link>
-            <ul className="flex space-x-6">
-              {links.map((link) => (
-                <li key={link.href}>
-                  <Link
-                    href={link.href}
-                    // 'classnames' is a function that takes an object in this object we specify the 'classes' we want to render and
-                    // the 'condition' in which they should be rendered. This will then return a 'string' that contains the 'classes' that
-                    // we want to render.
-                    className={classnames({
-                      "text-zinc-900": link.href === currentPath,
-                      "text-zinc-500": link.href !== currentPath,
-                      "hover:text-zinc-800 transition-colors": true,
-                    })}
-                  >
-                    {link.label}
-                  </Link>
-                </li>
-              ))}
-            </ul>
+            <NavLinks />
           </Flex>
-          <Box>
-            {status === "authenticated" && (
-              <DropdownMenu.Root>
-                <DropdownMenu.Trigger>
-                  <Avatar src={session.user!.image!} fallback="?" radius="full" className="cursor-pointer" />
-                </DropdownMenu.Trigger>
-                <DropdownMenu.Content align="end">
-                  <DropdownMenu.Label>
-                    <Text size="2">{session.user!.email}</Text>
-                  </DropdownMenu.Label>
-                  <DropdownMenu.Item>
-                    <Link href="/api/auth/signout">Log out</Link>
-                  </DropdownMenu.Item>
-                </DropdownMenu.Content>
-              </DropdownMenu.Root>
-            )}
-            {status === "unauthenticated" && <Link href="/api/auth/signin">Login</Link>}
-          </Box>
+          <AuthStatus />
         </Flex>
       </Container>
     </nav>
+  );
+};
+
+const NavLinks = () => {
+  // 'usePathname' is a Client Component hook that lets you read the current 'URL's' pathname.
+  const currentPath = usePathname();
+
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues/list" },
+  ];
+  return (
+    <ul className="flex space-x-6">
+      {links.map((link) => (
+        <li key={link.href}>
+          <Link
+            href={link.href}
+            // 'classnames' is a function that takes an object in this object we specify the 'classes' we want to render and
+            // the 'condition' in which they should be rendered. This will then return a 'string' that contains the 'classes' that
+            // we want to render.
+            className={classnames({
+              "nav-link": true,
+              "!text-zinc-900": link.href === currentPath,
+            })}
+          >
+            {link.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const AuthStatus = () => {
+  // Accessing the session:
+  const { status, data: session } = useSession();
+
+  if (status === "loading") return null;
+
+  if (status === "unauthenticated")
+    return (
+      <Link className="nav-link" href="/api/auth/signin">
+        Login
+      </Link>
+    );
+
+  return (
+    <Box>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Avatar src={session!.user!.image!} fallback="?" radius="full" className="cursor-pointer" />
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content align="end">
+          <DropdownMenu.Label>
+            <Text size="2">{session!.user!.email}</Text>
+          </DropdownMenu.Label>
+          <DropdownMenu.Item>
+            <Link href="/api/auth/signout">Log out</Link>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Box>
   );
 };
 
