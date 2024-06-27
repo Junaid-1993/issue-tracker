82cfcede29e7802f37f547664de17500367b1816
junaid-1993 <junaidali9001@gmail.com>
2024-06-27 14:49:27 +0500

    Refactor AssigneeSelect component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -9,34 +9,24 @@ import { Issue } from "@prisma/client";
 import toast, { Toaster } from "react-hot-toast";
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
-  const {
-    data: users,
-    error,
-    isLoading,
-  } = useQuery<User[]>({
-    queryKey: ["users"],
-    queryFn: () => axios.get("/api/users").then((res) => res.data),
-    staleTime: 60 * 1000, // 60s
-    retry: 3,
-  });
+  const { data: users, error, isLoading } = useUsers();
 
   if (isLoading) return <Skeleton />;
 
   if (error) return null;
 
+  const assignIssue = async (userId: string) => {
+    try {
+      await axios.patch("/api/issues/" + issue.id, { assignedToUserId: userId === "unassigned" ? null : userId });
+      toast.success("Changes saved successfully.");
+    } catch (error) {
+      toast.error("Changes could not be saved.");
+    }
+  };
+
   return (
     <>
-      <Select.Root
-        defaultValue={issue.assignedToUserId || "unassigned"}
-        onValueChange={async (userId) => {
-          try {
-            await axios.patch("/api/issues/" + issue.id, { assignedToUserId: userId === "unassigned" ? null : userId });
-            toast.success("Changes saved successfully.");
-          } catch (error) {
-            toast.error("Changes could not be saved.");
-          }
-        }}
-      >
+      <Select.Root defaultValue={issue.assignedToUserId || "unassigned"} onValueChange={assignIssue}>
         <Select.Trigger placeholder="Assign..." />
         <Select.Content>
           <Select.Group>
@@ -54,5 +44,13 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
     </>
   );
 };
+
+const useUsers = () =>
+  useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    staleTime: 60 * 1000, // 60s
+    retry: 3,
+  });
 
 export default AssigneeSelect;
